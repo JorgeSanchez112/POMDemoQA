91d1ccf65be16f11936f29b6411faee8dc8727eb
Jorge Sanchez <halo3ratons@gmail.com>
2024-01-24 19:57:46 -0500

    DOCS: Forty-second day

--- src/main/java/Pages/BSProfilePage.java
+++ src/main/java/Pages/BSProfilePage.java
@@ -6,11 +6,53 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class BSProfilePage extends BasePages {
     @FindBy(className = "main-header")
     WebElement title;
     @FindBy(id = "notLoggin-label")
     WebElement doNotLoginMessage;
+    @FindBy(id = "userName-label")
+    WebElement booksLabel;
+    @FindBy(id = "searchBox")
+    WebElement searchBox;
+    @FindBy(css = ".text-right > #userName-label")
+    WebElement usernameLabel;
+    @FindBy(id = "userName-value")
+    WebElement usernameValue;
+    @FindBy(css = ".text-right.col-sm-12 > #submit")
+    WebElement logOutButton;
+    @FindBy(className = "rt-th")
+    List<WebElement> tableHeaderTitles;
+    @FindBy(css = ".rt-tr > .rt-td:nth-child(1)")
+    List<WebElement> columnImages;
+    @FindBy(css = ".rt-tr > .rt-td:nth-child(2)")
+    List<WebElement> columnTitles;
+    @FindBy(css = ".rt-tr > .rt-td:nth-child(3)")
+    List<WebElement> columnAuthors;
+    @FindBy(css = ".rt-tr > .rt-td:nth-child(4)")
+    List<WebElement> columnPublishers;
+    @FindBy(css = ".rt-tr > .rt-td:nth-child(5)")
+    List<WebElement> columnActions;
+    @FindBy(className = "rt-noData")
+    WebElement messageNoData;
+    @FindBy(css = ".-previous > button")
+    WebElement previousButton;
+    @FindBy(css = ".-center > .-pageInfo")
+    WebElement pageTextOfCenterTable;
+    @FindBy(className = "-pageJump")
+    WebElement pageValue;
+    @FindBy(className = "-totalPages")
+    WebElement totalPagesNumber;
+    @FindBy(css = ".-next > button")
+    WebElement nextButton;
+    @FindBy(id = "gotoStore")
+    WebElement goToBookStoreButton;
+    @FindBy(css = ".text-center > button")
+    WebElement deleteAccountButton;
+    @FindBy(css = ".text-right.di > button")
+    WebElement deleteAllBooksButton;
 
     public BSProfilePage(WebDriver driver) {
         super(driver);
